52d1682519d2dcf0727365196f687fe90c2af329
Bruce Ackley <bruceackley@Bruces-MacBook-Pro.local>
2024-11-03 23:47:34 -0600

    WIP - new chord data structure

--- data/chords.sql
+++ data/chords.sql
@@ -1,37 +1,19 @@
-INSERT INTO chords (function, notes_rel_c3, scale, region)
-VALUES ( -- Major Key Simple Chords
-    'I',
-    'C3 E3 G3 B3 D4',
-    'major',
-    'tonic'
-), (
-    'ii',
-    'D3 F3 A3 C4 E4',
-    'major',
-    'subdominant'
-), (
-    'iii',
-    'E3 G3 B3 D4 F4',
-    'major',
-    'tonic dominant'
-), (
-    'IV',
-    'F3 A3 C4 E4 G4',
-    'major',
-    'subdominant'
-), (
-    'V',
-    'G3 B3 D4 F4 A4',
-    'major',
-    'dominant'
-), (
-    'vi',
-    'A3 C4 E4 G4 B4',
-    'major',
-    'tonic'
-), (
-    'vii',
-    'B3 D4 F4 A4 C5',
-    'major',
-    'dominant'
-);
+INSERT INTO chords_c3 
+(name_c3, function, mood, region, root_c3, intervals, outbound, map_mode) VALUES 
+-- Basic Diatonic, Tonic
+('C', 'I', 'major', 'tonic', 'C3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+('Cm', 'I-', 'minor', 'tonic', 'C3', '[0, 3, 7]', '[]', 'basic_diatonic'), 
+('Eb', 'bIII', 'major', 'tonic', 'Eb3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+('Am7b5', 'VI-7b5', 'half_diminished', 'tonic', 'A3', '[0, 3, 6, 10]', '[]', 'basic_diatonic'), 
+('Am', 'VI-', 'minor', 'tonic', 'A3', '[0, 3, 7]', '[]', 'basic_diatonic'), 
+-- Basic Diatonic, Subdominant
+('F', 'IV', 'major', 'subdominant', 'F3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+('Dm', 'II-', 'minor', 'subdominant', 'D3', '[0, 3, 7]', '[]', 'basic_diatonic'), 
+('Fm', 'IV-', 'minor', 'subdominant', 'F3', '[0, 3, 7]', '[]', 'basic_diatonic'), 
+('Dm7b5', 'II-7b5', 'half_diminished', 'subdominant', 'D3', '[0, 3, 6, 10]', '[]', 'basic_diatonic'), 
+('Ab', 'bVI', 'major', 'subdominant', 'Ab3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+-- Basic Diatonic, Dominant
+('Bb', 'bVII', 'major', 'dominant', 'Bb3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+('G', 'V', 'major_and_minor', 'dominant', 'G3', '[0, 4, 7]', '[]', 'basic_diatonic'), 
+('Bdim', 'VII-dim', 'diminished', 'dominant', 'B3', '[0, 3, 6, 9]', '[]', 'basic_diatonic'), 
+('Bm7b5', 'VII-7b5', 'half_diminished', 'dominant', 'B3', '[0, 3, 6, 10]', '[]', 'basic_diatonic');
---
+++ data/chords_backup.sql
@@ -0,0 +1,83 @@
+INSERT INTO chords (function, notes_rel_c3, scale, region)
+VALUES ( -- Major Key Simple Chords
+    'I',
+    'C3 E3 G3 B3 D4',
+    'major',
+    'tonic'
+), (
+    'ii',
+    'D3 F3 A3 C4 E4',
+    'major',
+    'subdominant'
+), (
+    'iii',
+    'E3 G3 B3 D4 F4',
+    'major',
+    'tonic dominant'
+), (
+    'IV',
+    'F3 A3 C4 E4 G4',
+    'major',
+    'subdominant'
+), (
+    'V',
+    'G3 B3 D4 F4 A4',
+    'major',
+    'dominant'
+), (
+    'vi',
+    'A3 C4 E4 G4 B4',
+    'major',
+    'tonic'
+), (
+    'vii',
+    'B3 D4 F4 A4 C5',
+    'major',
+    'dominant'
+),
+
+-- test (Need 2) 
+-- E maj 7
+( -- E maj 7
+    'E7',
+    'E3 G#3 B3 D#4',
+    'major',
+    'dominant'
+),
+
+(
+    'Amaj9b5',
+    'A3 C#3 D#3 G#3 A4',
+    'major',
+    'dominant'
+), 
+
+-- (
+--     'B7',
+--     'B3 D#4 F#4 A4',
+--     'major',
+--     'dominant'
+-- ), (
+--     'C7',
+--     'C3 E3 G3 Bb3',
+--     'major',
+--     'dominant'
+-- ), (
+--     'D7',
+--     'D3 F#3 A3 C4',
+--     'major',
+--     'dominant'
+-- ), (
+--     'F7',
+--     'F3 A3 C3 Eb3',
+--     'major',
+--     'dominant'
+-- ), (
+--     'G7',
+--     'G3 B3 D3 F3',
+--     'major',
+--     'dominant'
+-- ), 
+-- A maj 9 b5
+;
+
